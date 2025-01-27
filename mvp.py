a0f1d54922df115be52480b352dee83f2e17557c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-01-26 20:11:44 -0600

    ran uv init

--- mvp.py
+++ mvp.py
@@ -9,16 +9,17 @@
 import requests
 import argparse
 import subprocess
+from typing import Dict, Any
 
 
-def run_manim_code(code):
+def run_manim_code(code: str) -> None:
     with open("generated_code.py", "w") as f:
         f.write(code)
     subprocess.run(["manim", "-pql", "generated_code.py"])
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(
+    parser: argparse.ArgumentParser = argparse.ArgumentParser(
         description="A CLI tool for converting natural language to Manim animations."
     )
     parser.add_argument(
@@ -27,9 +28,9 @@ def main() -> None:
         help="What you want animated.",
         required=True,
     )
-    args = parser.parse_args()
+    args: argparse.Namespace = parser.parse_args()
 
-    GEMINI_URL = f"""
+    GEMINI_URL: str = f"""
     https://nova-motors-server.vercel.app/gemini?prompt=
 Your sole purpose is to convert natural language into Manim (a Python library for creating mathematical animations) code. 
 You will be given some text and must write Manim code to the best of your abilities. DO NOT OUTPUT MARKDOWN CODE, JUST PYTHON CODE.
@@ -39,7 +40,7 @@ AGAIN, DO NOT OUTPUT ANYTHING OTHER THAN VALID PYTHON + MANIM CODE. This is the 
     print(
         "Calling Google Gemini now. Please note that since I'm hosting my Flask app on Vercel, there might be a cold start, so this may take a while.\n"
     )
-    response = requests.get(GEMINI_URL).json()
+    response: Dict[str, Any] = requests.get(GEMINI_URL).json()
 
     code: str = response["candidates"][0]["content"]["parts"][0]["text"]
     code = "\n".join(code.splitlines()[1:-1])
