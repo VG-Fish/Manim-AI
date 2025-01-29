d70aa886d0ace88275475ba23f62b2591ca67a3c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-01-29 15:12:32 -0600

    added comments and debug capabilities to parser.py

--- cst_parser.py
+++ cst_parser.py
@@ -10,13 +10,29 @@ from typing import Self
 from libcst.display import dump
 import libcst.matchers as m
 
-class Visitor(cst.CSTTransformer):
-    def __init__(self, sound_file_path: str):
+
+class GeminiTransformer(cst.CSTTransformer):
+    """
+    A class to add code to the Gemini generated code.
+    """
+    def __init__(
+        self,
+        sound_file_path: str,
+        debug: bool = False,
+        debug_file_path: str = "",
+        num_new_lines: int = 0,
+    ):
         self.sound_file_path = sound_file_path
-    
+        self.debug = debug
+        self.debug_file_path = debug_file_path
+        self.num_new_lines = num_new_lines
+
     def leave_FunctionDef(
         self: Self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef
     ) -> cst.FunctionDef:
+        """
+        This function adds `self.interactive_embed()` to the end of the construct function.
+        """
         if original_node.name.value != "construct":
             return super().leave_FunctionDef(original_node, updated_node)
 
@@ -31,39 +47,38 @@ class Visitor(cst.CSTTransformer):
         original_node: cst.SimpleStatementLine,
         updated_node: cst.SimpleStatementLine,
     ) -> cst.SimpleStatementLine:
+        """
+        This function adds `self.add_sound(...)` after certain Manim function calls, such as `Create()` or `FadeOut()`.
+        """
         for child in original_node.children:
-            node_matches = m.matches(
-                child, 
+            # For debugging purposes.
+            if self.debug:
+                with open(self.debug_file_path, "w") as f:
+                    f.write(dump(child))
+                    f.write("\n" * self.num_new_lines)
+
+            # These if statement(s) match specific nodes to add `self.add_sound(...)` after lines containing certain Manim function calls.
+            if m.matches(
+                child,
                 m.Expr(
                     value=m.Call(
                         args=[
                             m.ZeroOrMore(m.Arg()),
-                            m.Arg(
-                                value=m.Call(
-                                    func=m.Name(
-                                        value="Create"
-                                    )
-                                )
-                            ),
+                            m.Arg(value=m.Call(func=m.Name(value="Create"))),
                             m.ZeroOrMore(m.Arg()),
                         ],
                     )
+                ),
+            ):
+                code_to_add: cst.Module = cst.parse_statement(
+                    f"self.add_sound('{self.sound_file_path}')"
                 )
-            )
-            if not node_matches:
-                continue
-            
-            code_to_add: cst.Module = cst.parse_statement(
-                f"self.add_sound('{self.sound_file_path}')"
-            )
-            return cst.FlattenSentinel([
-                updated_node,
-                code_to_add
-            ])
+                return cst.FlattenSentinel([updated_node, code_to_add])
 
         return super().leave_SimpleStatementLine(original_node, updated_node)
 
-def add_interactivity() -> None: 
+
+def add_interactivity() -> None:
     """
     Adds interactivity to the generated Gemini code.
     """
@@ -71,7 +86,7 @@ def add_interactivity() -> None:
         code: str = f.read()
     code: cst.Module = cst.parse_module(code)
 
-    updated_cst = code.visit(Visitor("Up_bend_250ms.wav"))
+    updated_cst = code.visit(GeminiTransformer("Up_bend_250ms.wav", True, "parser_debug.txt"))
     with open("generated_code.py", "w") as f:
         f.write(updated_cst.code)
 
