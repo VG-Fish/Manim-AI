c5995acedb0cde8a2b2bb68c8074ad56b60f049c
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2025-04-29 09:16:49 -0500

    updated package to expose a generate video function

--- src/mAInim/mvp.py
+++ src/mAInim/mvp.py
@@ -46,7 +46,7 @@ def main() -> None:
 
     print("Getting response...")
 
-    PROMPT = f"""Your sole purpose is to convert natural language into Manim code. 
+    PROMPT: str = f"""Your sole purpose is to convert natural language into Manim code. 
 You will be given some text and must write valid Manim code to the best of your abilities.
 DON'T code bugs and SOLELY OUTPUT PYTHON CODE. Import ALL the necessary libraries.
 Define ALL constants. After you generate your code, check to make sure that it can run.
@@ -74,6 +74,35 @@ The prompt: {args.prompt}"""
     print("Creating the interactive scene...")
     run_manim_code(code)
 
+def generate_video(prompt: str) -> None:
+    GEMINI_URL: str = "https://gemini-wrapper-nine.vercel.app/gemini"
 
-if __name__ == "__main__":
-    main()
+    print("Getting response...")
+
+    PROMPT: str = f"""Your sole purpose is to convert natural language into Manim code. 
+You will be given some text and must write valid Manim code to the best of your abilities.
+DON'T code bugs and SOLELY OUTPUT PYTHON CODE. Import ALL the necessary libraries.
+Define ALL constants. After you generate your code, check to make sure that it can run.
+Ensure all the generated manim code is compatible with manim 0.19.0. DO NOT USE
+DEPRECATED CLASSES, such as "ParametricSurface." Ensure EVERY element in the scene is visually distinctive. 
+Define EVERY function you use. Write text at the top to explain what you're doing.
+REMEMBER, YOU MUST OUTPUT CODE THAT DOESN'T CAUSE BUGS. ASSUME YOUR CODE IS BUGGY, AND RECODE IT AGAIN.
+The prompt: {prompt}"""
+
+    response: Response | Dict[str, str] = post(GEMINI_URL, json={"prompt": PROMPT})
+
+    if not response:
+        print("Couldn't connect to the backend to generate the code.")
+        return
+
+    json: Dict = response.json()
+
+    if "error" in json:
+        print(json["error"])
+        return
+
+    code: str = json["output"]
+    code = "\n".join(code.splitlines()[1:-1])
+
+    print("Creating the interactive scene...")
+    run_manim_code(code)
